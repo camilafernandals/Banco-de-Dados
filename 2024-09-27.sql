3fd4781dd63f5f274db239e5dde510348787d9d6
Camila Fernanda <163914073+camilafernandals@users.noreply.github.com>
2024-09-27 18:09:08 -0300

    Add files via upload

---
+++ 2024-09-27.sql
@@ -0,0 +1,144 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 27-Set-2024 às 23:09
+-- Versão do servidor: 10.4.25-MariaDB
+-- versão do PHP: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db_biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `emprestimos`
+--
+
+CREATE TABLE `emprestimos` (
+  `CodEmprestimo` int(11) NOT NULL,
+  `Data_Emprestimo` date DEFAULT NULL,
+  `Data_Devolucao` date DEFAULT NULL,
+  `CodLivro` int(11) DEFAULT NULL,
+  `CodLeitor` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `leitores`
+--
+
+CREATE TABLE `leitores` (
+  `CodLeitor` int(11) NOT NULL,
+  `Nome` varchar(255) DEFAULT NULL,
+  `DtNasc` date DEFAULT NULL,
+  `Celular` varchar(255) DEFAULT NULL,
+  `Email` varchar(255) DEFAULT NULL,
+  `RA` int(11) DEFAULT NULL,
+  `Endereco` varchar(255) DEFAULT NULL,
+  `NumEnd` varchar(255) DEFAULT NULL,
+  `Bairro` varchar(255) DEFAULT NULL,
+  `CidadeUF` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `leitores`
+--
+
+INSERT INTO `leitores` (`CodLeitor`, `Nome`, `DtNasc`, `Celular`, `Email`, `RA`, `Endereco`, `NumEnd`, `Bairro`, `CidadeUF`) VALUES
+(1, 'João Silva Sauro', '2001-04-01', '(11)98788-2311', 'joao@hotmail.com', 631113101, 'Rua Boa Esperança', '201', 'Janga', 'Paulista/PE'),
+(2, 'Maria Silva Nascimento', '2011-07-21', '(11)98788-2311', 'maria@hotmail.com', 631113102, 'Rua da Vovó', '101', 'MAranguape I', 'Paulista/PE'),
+(3, 'Mateus Novaes Abreu', '2012-06-11', '(11)98788-2311', 'mateus@hotmail.com', 631113105, 'Rua da Praia', '41', 'Bosque 2', 'Olinda/PE'),
+(4, 'Joana Maria de Souza', '2008-05-31', '(11)98788-2311', 'joana@hotmail.com', 631113155, 'Rua da Casa', '121', 'Bosque 1', 'Olinda/PE'),
+(5, 'Maria  das Graças Menezes', '2007-11-12', '(11)98788-2311', 'maria@hotmail.com', 631113121, 'Avenida dos Lirios', '78', 'Boa Viagem', 'Recife/PE');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livros`
+--
+
+CREATE TABLE `livros` (
+  `CodLivro` int(11) NOT NULL,
+  `Titulo` varchar(255) DEFAULT NULL,
+  `Autor` varchar(255) DEFAULT NULL,
+  `Editora` varchar(255) DEFAULT NULL,
+  `Sinopse` varchar(255) DEFAULT NULL,
+  `AnoPublicacao` int(11) DEFAULT NULL,
+  `Genero` varchar(255) DEFAULT NULL,
+  `Paginas` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `livros`
+--
+
+INSERT INTO `livros` (`CodLivro`, `Titulo`, `Autor`, `Editora`, `Sinopse`, `AnoPublicacao`, `Genero`, `Paginas`) VALUES
+(1, '1984', 'George Orwell', 'Garnier', 'Winston, herói de 1984, último romance de George Orwell, vive aprisionado na engrenagem totalitária de uma sociedade completamente dominada pelo Estado, onde tudo é feito coletivamente, mas cada qual vive sozinho.', 1949, 'Romance Distópico', 416),
+(2, 'Viagem ao Centro da Terra', 'Júlio Verne', 'Globo', 'O romance narra a aventura de um professor de geologia, Otto Lidenbrock, que descobre um manuscrito antigo que revela a existência de um caminho para o centro da Terra. Juntamente com seu sobrinho Axel e um guia chamado Hans, eles embarcam em uma expediçã', 1864, 'Ficção científica', 300),
+(3, 'O Morro dos Ventos Uivantes', 'Emily Brontë', 'Penguin', 'O romance acompanha a tumultuada relação entre Heathcliff e Catherine Earnshaw, ambientada em uma isolada propriedade inglesa chamada Morro dos Ventos Uivantes. A história explora temas de amor, vingança e a força destrutiva das paixões humanas, à medida ', 1847, 'Romance', 400),
+(4, 'Orgulho e Preconceito', 'Jane Austen', 'Zahar', 'O romance segue a história de Elizabeth Bennet, uma jovem inteligente e espirituosa, que navega pelas complexidades do amor, classe social e preconceitos na Inglaterra do século XIX. Ao conhecer o orgulhoso Sr. Darcy, ela se vê desafiada a reconsiderar su', 1813, 'Romance', 400),
+(5, 'Revolução dos Bichos', 'George Orwell', 'Companhia das Letras', 'Esta fábula política satiriza a corrupção do idealismo revolucionário. Na Fazenda do Solar, os animais se rebelam contra seus opressores humanos, buscando igualdade e liberdade. No entanto, a revolução se torna um regime autoritário liderado pelos porcos,', 1945, 'Fábula', 150);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD PRIMARY KEY (`CodEmprestimo`),
+  ADD KEY `CodLivro` (`CodLivro`),
+  ADD KEY `CodLeitor` (`CodLeitor`);
+
+--
+-- Índices para tabela `leitores`
+--
+ALTER TABLE `leitores`
+  ADD PRIMARY KEY (`CodLeitor`);
+
+--
+-- Índices para tabela `livros`
+--
+ALTER TABLE `livros`
+  ADD PRIMARY KEY (`CodLivro`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  MODIFY `CodEmprestimo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `emprestimos`
+--
+ALTER TABLE `emprestimos`
+  ADD CONSTRAINT `emprestimos_ibfk_1` FOREIGN KEY (`CodLivro`) REFERENCES `livros` (`CodLivro`),
+  ADD CONSTRAINT `emprestimos_ibfk_2` FOREIGN KEY (`CodLeitor`) REFERENCES `leitores` (`CodLeitor`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
